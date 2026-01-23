d3ffdd5da2b89f4f87ce7c937a7e6d0ad97f7c05
Tanakorn Khunkhao <tanakorn100@hotmail.com>
2026-01-24 01:57:44 +0700

    reformat

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,18 +3,18 @@
  * Application settings and constants
  */
 
-import { AppConfig } from '../types';
+import { AppConfig } from "../types";
 
-const appConfig: AppConfig['app'] = {
+const appConfig: AppConfig["app"] = {
   // Time intervals (in milliseconds)
-  airconPowerOffDuration: 300000,      // 5 minutes - delay before turning off aircons
-  eventProcessInterval: 10000,         // 10 seconds - main event loop interval
-  dataPushingInterval: 300000,         // 5 minutes - MongoDB save interval
+  airconPowerOffDuration: 300000, // 5 minutes - delay before turning off aircons
+  eventProcessInterval: 5000, // 5 seconds - main event loop interval
+  dataPushingInterval: 300000, // 5 minutes - MongoDB save interval
 
   // Device counts
-  rpiCount: 2,                         // Number of Raspberry Pi cameras
-  airconControllerCount: 3,            // Number of aircon controllers
-  lightingControllerCount: 1           // Number of lighting controllers
+  rpiCount: 2, // Number of Raspberry Pi cameras
+  airconControllerCount: 3, // Number of aircon controllers
+  lightingControllerCount: 1, // Number of lighting controllers
 };
 
 export default appConfig;
