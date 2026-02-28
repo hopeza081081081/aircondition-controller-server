fcafe4c57704dc7431dc0b775de809a2e6001380
Tanakorn Khunkhao <tanakorn100@hotmail.com>
2026-02-28 18:40:52 +0700

    increase turning off aircons delay to 15 minutes.

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -7,7 +7,7 @@ import { AppConfig } from "../types";
 
 const appConfig: AppConfig["app"] = {
   // Time intervals (in milliseconds)
-  airconPowerOffDuration: 300000, // 5 minutes - delay before turning off aircons
+  airconPowerOffDuration: 900000, // 15 minutes - delay before turning off aircons
   eventProcessInterval: 5000, // 5 seconds - main event loop interval
   dataPushingInterval: 300000, // 5 minutes - MongoDB save interval
 
